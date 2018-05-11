83f0891d2bddc6e161e9e5a01aef6994d61b1c73
He Zhang <rbczhang90@gmail.com>
2018-05-11 16:22:29 -0700

    Create sun animation

--- app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
+++ app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
@@ -1,8 +1,17 @@
 package signalcat.github.com.smartsunlamp;
+import android.animation.ObjectAnimator;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -13,6 +22,7 @@ import signalcat.github.com.smartsunlamp.httpResponseHandler.SunHttpResponseHand
 public class SunActivity extends AppCompatActivity {
 
     private SunTime sunTime;
+    private ImageView sunShape;
     private AsyncHttpClient client = new AsyncHttpClient();
 
     @Override
@@ -23,7 +33,7 @@ public class SunActivity extends AppCompatActivity {
         sunTime = new SunTime();
         //Button btnGetSunTime = findViewById(R.id.btn_getSunTime);
         final TextView tvSunRiseTime = findViewById(R.id.tv_sunRiseTime);
-
+        sunShape = findViewById(R.id.iv_sun);
         getSunTime("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400", new Runnable() {
             @Override
             public void run() {
@@ -31,7 +41,18 @@ public class SunActivity extends AppCompatActivity {
             }
         });
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Path path = new Path();
+            path.arcTo(20f,50f,600f,600f,180f,180f,true);
+            ObjectAnimator animator = ObjectAnimator.ofFloat(sunShape, View.X, View.Y, path);
+            animator.setDuration(2000);
+            animator.start();
+
+        }
+
     }
+
+
 
     public void getSunTime(String url, Runnable runnable){
         SunHttpResponseHandler handler = new SunHttpResponseHandler(sunTime, runnable);
