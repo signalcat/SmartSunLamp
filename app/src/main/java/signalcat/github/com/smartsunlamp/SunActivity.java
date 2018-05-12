42361d85c3e3368552e4a75836ef5b87a85753ab
He Zhang <rbczhang90@gmail.com>
2018-05-11 18:20:47 -0700

    Change sun shape

--- app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
+++ app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
@@ -9,6 +9,7 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
