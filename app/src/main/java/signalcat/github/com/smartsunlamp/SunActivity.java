8e7674771854d9c71be4adcb792c6eecd1d1b04f
He Zhang <rbczhang90@gmail.com>
2018-05-16 00:00:58 -0700

    Move sun to the right location

--- app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
+++ app/src/main/java/signalcat/github/com/smartsunlamp/SunActivity.java
@@ -48,9 +48,10 @@ public class SunActivity extends AppCompatActivity {
                 tvSunRiseTime.setText(sunTime.getSunRise());
                 tvSunSetTime.setText(sunTime.getSunSet());
                 float angle = getSunLocation(sunTime.getSunRise(),sunTime.getSunSet());
+                Log.e("angle", String.valueOf(angle));
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     Path path = new Path();
-                    path.arcTo(20f,50f,600f,600f,180f,180f,true);
+                    path.arcTo(20f,50f,600f,600f,180f,angle,true);
                     ObjectAnimator animator = ObjectAnimator.ofFloat(sunShape, View.X, View.Y, path);
                     animator.setDuration(1000);
                     animator.start();
@@ -75,29 +76,37 @@ public class SunActivity extends AppCompatActivity {
     public float getSunLocation(String sunRiseTime, String sunSetTime) {
         float angle = 0;
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        String curTime = sdf.format(new Date());
+//        String curTime = sdf.format(new Date());
+        String curTime = "08:11:11";
         String[] parseResult = curTime.split(":");
         float curTimeInMin = Integer.valueOf(parseResult[0]) * 60 + Integer.valueOf(parseResult[1]);
 
         String[] sunRiseParsed = sunRiseTime.split(" ");
         String[] sunRiseParsedInMin = sunRiseParsed[0].split(":");
-        float sunRiseInMin = Integer.valueOf(sunRiseParsedInMin[0]) * 60 + Integer.valueOf(sunRiseParsedInMin[1]);
+        float sunRiseInMin;
+        if (sunRiseParsed[1].equals("AM")) {
+            sunRiseInMin = Integer.valueOf(sunRiseParsedInMin[0]) * 60 + Integer.valueOf(sunRiseParsedInMin[1]);
+        } else {
+            sunRiseInMin = (Integer.valueOf(sunRiseParsedInMin[0]) + 12) * 60 + Integer.valueOf(sunRiseParsedInMin[1]);
+        }
 
         String[] sunSetParsed = sunSetTime.split(" ");
         String[] sunSetParsedInMin = sunSetParsed[0].split(":");
-        float sunSetInMin = Integer.valueOf(sunSetParsedInMin[0]) * 60 + Integer.valueOf(sunSetParsedInMin[1]);
+        float sunSetInMin;
+        if (sunSetParsed[1].equals("AM")) {
+            sunSetInMin = Integer.valueOf(sunSetParsedInMin[0]) * 60 + Integer.valueOf(sunSetParsedInMin[1]);
+        } else {
+            sunSetInMin = (Integer.valueOf(sunSetParsedInMin[0]) + 12) * 60 + Integer.valueOf(sunSetParsedInMin[1]);
+        }
 
         Log.e("curTime", String.valueOf(curTimeInMin));
         Log.e("sunrise", String.valueOf(sunRiseInMin));
         Log.e("sunset", String.valueOf(sunSetInMin));
-
-//        Date sunRiseTime;
-//        Date sunSetTime;
-//        float totalTime = sunSetTime - sunRiseTime;
-//        if (curTime >= sunRiseTime && curTime <= sunSetTime) {
-//            // Assume the sun is moving at a fixed speed
-//            angle = ((curTime - sunRiseTime) / totalTime) * 180;
-//        }
+        float totalTime = sunSetInMin - sunRiseInMin;
+        if (curTimeInMin >= sunRiseInMin && curTimeInMin <= sunSetInMin) {
+            // Assume the sun is moving at a fixed speed
+            angle = ((curTimeInMin - sunRiseInMin) / totalTime) * 180;
+        }
         return angle;
     }
 
